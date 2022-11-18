71a10da658a28152ab4c7f7558816fb4e37d7548
Rasia <manoj1chaurasia2@gmail.com>
2022-11-18 15:41:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,14 +8,3 @@ output "name" {
   description = "Name (id) of the bucket"
   value       = aws_s3_bucket.s3_bucket.id
 }
-/*
-output "domain" {
-  description = "Domain Name of the bucket"
-  value       = aws_s3_bucket.s3_bucket.website_domain
-}
-
-output "endpoint" {
-  description = "Endpoint Information of the bucket"
-  value       = aws_s3_bucket.s3_bucket.website_endpoint
-}
-*/
